fdb74b04691dc39152be300cff4c0da24ca13fcd
skbhati199@gmail.com <skbhati199@gmail.com>
2021-07-11 23:14:41 +0530

    final

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var in_client_id = '134461202039487',
 		in_client_secret = '6d38c040c094b15843e60eca3a9bcf56',
-		in_redirect_uri = 'https://emojizonedemo.herokuapp.com/auth/instagram/callback',
+		in_redirect_uri = 'https://dry-oasis-17983.herokuapp.com/auth/instagram/callback',
 		in_auth_url = 'https://api.instagram.com/oauth/authorize/?client_id=' + in_client_id + '&redirect_uri=' + in_redirect_uri + '&response_type=code&scope=user_profile'
 
 var db_uri = 'mongodb://localhost:27017/instagram-auth';
